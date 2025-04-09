54a32032ab4021c9137c82b4c29addd1ff816edd
jagafaster11 <jagadeeshkanna1997@gmail.com>
2025-04-09 14:12:09 +0530

    footer_changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -15,10 +16,10 @@ const Footer = () => {
                 <div>
                     <p className='text-xl font-medium mb-5'>COMPANY</p>
                     <ul className='flex flex-col gap-1 text-gray-600'>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Delivery</li>
-                        <li>Privacy Policy</li>
+                        <Link to='/'>Home</Link>
+                        <Link to='/about'>About Us</Link>
+                        <Link to='/orders'>Delivery</Link>
+                        <Link to='/'>Privacy Policy</Link>
                     </ul>
                 </div>
 
